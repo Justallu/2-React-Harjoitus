e8791869cc621ac6bed70157ca8dfad943309102
Alpertti Fält <Alpertti.Flt@kukkula.local>
2025-01-21 09:47:33 +0200

    Käännös

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React testiprojekti!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          luku on {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          muokkaa <code>src/App.jsx</code> ja tallenna muutokset HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Klikkaa Reactia ja Viten logoa saadaksesi lisätietoa
       </p>
     </>
   )
